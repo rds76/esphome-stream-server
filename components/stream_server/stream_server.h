df37ceaaf55a0d9b5c59cfaa22c30924b69a425f
Radim <admin@radas.net>
2026-03-26 09:54:40 +0100

    x

--- components/stream_server/stream_server.h
+++ components/stream_server/stream_server.h
@@ -82,6 +82,10 @@ protected:
     std::unique_ptr<uint8_t[]> buf_{};
     size_t buf_head_{0};
     size_t buf_tail_{0};
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2026, 3, 0)
+    std::unique_ptr<esphome::socket::ListenSocket> socket_{};
+#else
     std::unique_ptr<esphome::socket::Socket> socket_{};
+#endif         
     std::vector<Client> clients_{};
 };
